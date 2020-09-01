1db33905708d72d3fc32bbb119927d1ba696ee57
zzzzz <chenze.zhao0@outlook.com>
2020-09-01 08:43:25 -0700

    add default values to columns

--- reset_tables.sql
+++ reset_tables.sql
@@ -7,11 +7,11 @@ DROP TABLE IF EXISTS user_projects;
 DROP TABLE IF EXISTS bugs;
 
 CREATE TABLE users (
-    username varchar(255),
-    password varchar(16), -- AES-512 cipher text length
-    salt int,
-    first_name TEXT,
-    last_name TEXT,
+    username varchar(255) NOT NULL,
+    password varchar(16) NOT NULL, -- AES-512 cipher text length
+    salt int NOT NULL,
+    first_name TEXT DEFAULT '',
+    last_name TEXT DEFAULT '',
     PRIMARY KEY (username)
 );
 
@@ -24,18 +24,19 @@ CREATE TABLE sessions (
 
 CREATE TABLE projects (
   pid INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT, -- Project name
+  name TEXT NOT NULL, -- Project name
   owner varchar(255),
-  status varchar(6), -- ACTIVE or CLOSE
-  start_date varchar(10), -- MM-DD-YYYY
-  end_date varchar(19), -- MM-DD-YYYY hh:mm AM/PM
-  overview TEXT, -- Project overview
+  status varchar(6) DEFAULT 'ACTIVE', -- ACTIVE or CLOSE
+  start_date varchar(10) NOT NULL, -- YYYY-MM-DD
+  end_date varchar(19) DEFAULT 'N/A', -- YYYY-MM-DD hh:mm AM/PM
+  overview TEXT DEFAULT '', -- Project overview
   FOREIGN KEY (owner) REFERENCES users
 );
 
 CREATE TABLE user_projects (
   username varchar(255),
   pid INT,
+  notes TEXT DEFAULT '[]', -- JSON list
   FOREIGN KEY (username) REFERENCES users,
   FOREIGN KEY (pid) REFERENCES projects,
   PRIMARY KEY (username, pid)
@@ -43,13 +44,13 @@ CREATE TABLE user_projects (
 
 CREATE TABLE bugs (
   bid INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT, --bug title
+  title TEXT NOT NULL, --bug title
   reporter varchar(255),
-  created_date varchar(10), -- MM-DD-YYYY
-  status varchar(11), -- open/in progress/closed
+  created_date varchar(10) NOT NULL, -- YYYY-MM-DD
+  status varchar(11) DEFAULT 'OPEN', -- open/in progress/closed
   assignee varchar(255),
-  due_date varchar(19), -- MM-DD-YYYY hh:mm AM/PM
-  severity varchar(8), -- none/critical/major/minor
+  due_date varchar(19) DEFAULT 'N/A', -- YYYY-MM-DD hh:mm AM/PM
+  severity varchar(8) NOT NULL, -- none/critical/major/minor
   pid INTEGER,
   FOREIGN KEY (reporter) REFERENCES users,
   FOREIGN KEY (assignee) REFERENCES users,
